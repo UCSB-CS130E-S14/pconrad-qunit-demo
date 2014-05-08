3c798950c9a8f125adda0545773329e13fc02771
pconrad <pconrad@cs.ucsb.edu>
2014-05-08 15:52:38 -0700

    First working version

---
+++ js/funcs.js
@@ -0,0 +1,6 @@
+// funcs.js    Demo of test-driven-development in JavaScript
+
+function square(x) {
+    // should return x*x
+    return "stub"
+}
---
+++ tests/funcsTest.js
@@ -0,0 +1,6 @@
+test( "funcs", 3, function() {
+
+    equal( square(3), 9);
+    equal( square(-3), 9);
+    equal( square(0.5), 0.25);
+});
